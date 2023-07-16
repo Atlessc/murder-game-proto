dec6e217d61f787284b7805df19fdf0a1e89b8d4
Tyler Smith <tylersmith4596@gmail.com>
2023-07-16 01:22:28 -0700

    i ahve the logic and mechanics for setting up the game and tested with console log and function Test and having the player placed correctly in the correct room

--- src/component/Game.jsx
+++ src/component/Game.jsx
@@ -1,31 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import data from './GameItems.json';
+import './Game.css';
 
 const characters = data.Characters;
 const rooms = data.Rooms;
 const weapons = data.Weapons;
 
-console.log(characters.map(character => character.name));
-console.log(rooms.map(room => room.name));
-console.log(weapons.map(weapon => weapon.name));
+// console.log(characters.map(character => character.name));
+// console.log(rooms.map(room => room.name));
+// console.log(weapons.map(weapon => weapon.name));
+
+const CharacterRoomTracker = {
+    "Foyer": [],
+    "Dining Room": [],
+    "Kitchen": [],
+    "Art Studio": [],
+    "Library": [],
+    "Living Room": [],
+    "Master Bedroom": []
+  };
 
 const Game = () => {
 
-    const CharacterRoomTracker = {
-        kitchen: [],
-        library: [],
-        masterBedroom: [],
-        diningRoom: [],
-        foyer: [],
-        artStudio: [],
-        livingRoom: []
-    };
-
+    const [GameStart, setGameStart] = useState(false);
     const [msChar, setMsChar] = useState("");
     const [msRoom, setMsRoom] = useState("");
     const [msWeapon, setMsWeapon] = useState("");
-
-    const [playerRoomTracker, setPlayerRoomTracker] = useState("Foyer");
+    const [playerRoomTracker, setPlayerRoomTracker] = useState("");
+    const [player, setPlayer] = useState({ name: "Detective", room: "" });
 
     const playerCurrentRoom = playerRoomTracker;
 
@@ -33,8 +35,63 @@ const Game = () => {
         setPlayerRoomTracker(room);
     };
 
+    const PlaceCharactersInRooms = (msChar, msRoom) => {
+        // Create a copy of the characters array
+        const charactersCopy = [...characters];
+
+
+        // Find the index of msChar in the characters array
+        const msCharIndex = charactersCopy.findIndex(character => character.name === msChar);
+
+        // Remove msChar from the characters array
+        charactersCopy.splice(msCharIndex, 1);
+
+
+        // Shuffle the characters array
+        for (let i = charactersCopy.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [charactersCopy[i], charactersCopy[j]] = [charactersCopy[j], charactersCopy[i]];
+        }
+
+        // Place the first 8 characters in rooms that do not equal msRoom
+        const otherRooms = Object.keys(CharacterRoomTracker).filter(room => room !== msRoom);
+        for (let i = 0; i < 8; i++) {
+          const character = charactersCopy[i];
+          const roomIndex = i % otherRooms.length;
+          const roomName = otherRooms[roomIndex];
+          CharacterRoomTracker[roomName].push(character.name);
+        }
+        // Place the remaining character and msChar in msRoom
+        CharacterRoomTracker[msRoom].push(charactersCopy[8].name, msChar);
+      }
+
+
+      function InitializeGame() {
+        setGameStart(true);
+        const newMsChar = characters[Math.floor(Math.random() * characters.length)].name;
+        const newMsRoom = rooms[Math.floor(Math.random() * rooms.length)].name;
+        const newMsWeapon = weapons[Math.floor(Math.random() * weapons.length)].name;
+        setPlayer(player => ({ ...player, room: "Foyer" }));
+
+        setMsChar(newMsChar);
+        setMsRoom(newMsRoom);
+        setMsWeapon(newMsWeapon);
+        PlaceCharactersInRooms(newMsChar, newMsRoom);
+      }
+
+      function test() {
+        console.log(CharacterRoomTracker);
+        console.log(player.room);
+        console.log(msChar);
+        console.log(msRoom);
+        console.log(msWeapon);
+      };
+
     return(
-        null
+        <>
+        { GameStart ? null :
+        <div className="StartGameBtn" onClick={InitializeGame}>Start Game</div>}
+        </>
     )
 };
 
--- src/component/Overlay.jsx
+++ src/component/Overlay.jsx
@@ -12,6 +12,7 @@ export default function Overlay() {
         setShowActionMenu(!showActionMenu)
     }
 
+
     return (
         <div className="Overlay">
             <div className="POV"><Game /></div>
